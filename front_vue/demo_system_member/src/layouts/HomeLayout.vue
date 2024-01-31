52c9e6df4416826f20e5522851d18afd5f5c7552
tdedwin <nuttawut.c39@gmail.com>
2024-01-31 18:50:26 +0700

    add homelayout

---
+++ front_vue/demo_system_member/src/layouts/HomeLayout.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+
+</script>
+
+<template>
+    <div class="container mx-auto mt-6 flex justify-center">
+        <div class="navbar bg-base-100">
+            <div class="flex-1">
+                <RouterLink :to="{ name: 'home' }" class="btn btn-ghost text-xl">YOUR NAME COMPANY</RouterLink>
+            </div>
+            <div class="flex-none">
+                <ul class="menu menu-horizontal px-1">
+                    <li>
+                        <RouterLink :to="{name : 'login'}">เข้าสู่ระบบ</RouterLink>
+                    </li>
+                    <li>
+                        <RouterLink :to="{name : 'home'}">หน้าหลัก</RouterLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- Main Content -->
+    <slot></slot>
+
+    <footer class="footer footer-center p-4 bg-base-300 text-base-content">
+      <aside>
+        <p>Copyright © 2023 - All right reserved by YOUR NAME COMPANY</p>
+      </aside>
+    </footer>
+</template>
